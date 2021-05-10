90b5f998cf420041b6de3c61fbae855ac4144502
ayman-d <aymand1982@gmail.com>
2021-05-10 00:16:13 -0600

    Add methods to list search results

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,48 +1,63 @@
-import React, {useState, useEffect} from "react";
-import styled from 'styled-components';
-import lens from '../lens.jpg'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import lens from "../lens.jpg";
 import MovieDetails from "./MovieDetails";
 
 const Dashboard = () => {
-
   const [results, setResults] = useState([]);
-  const [nominations, setNominations] = useState([])
+  const [nominations, setNominations] = useState([]);
   const [input, setInput] = useState("");
 
   const url = "http://www.omdbapi.com/?apikey=81588e03&type=movie&s=";
 
-  const handleChange = async (e) => {
-    let userInput = e.target.value;
-    setInput(userInput);
-    
-    if (userInput.length > 2) {
-      const movieSearchResult = await fetch(url+userInput);
-      const movieSearchData = await movieSearchResult.json();
-      console.log(movieSearchData); 
-    }
-  } 
+  useEffect(async () => {
+    const movieSearchResult = await fetch(url + input);
+    const movieSearchData = await movieSearchResult.json();
 
+    if (movieSearchData.Search !== undefined) {
+      setResults(movieSearchData.Search);
+    } else {
+      setResults([]);
+    }
+  }, [input]);
 
   return (
     <DashboardStyles>
       <h2>The Shoppies</h2>
       <div className="search-box">
         <label htmlFor="searchText">Movie Title</label>
-        <input type="text" name="searchText" id="searchText" onChange={handleChange} />
+        <input
+          type="text"
+          name="searchText"
+          id="searchText"
+          onChange={(e) => setInput(e.target.value)}
+        />
       </div>
       <div className="actions-area">
         <div>
           <p className="search-string">Top 10 Results for {input}</p>
           <ul>
-            <MovieDetails />
-            <MovieDetails />
-            <MovieDetails />
+            {results.map((movie, index) => (
+              <MovieDetails
+                title={movie.Title}
+                year={movie.Year}
+                key={movie.imdbD}
+                list="results"
+              />
+            ))}
           </ul>
         </div>
         <div>
           <p>Nominations</p>
           <ul>
-            <MovieDetails />
+            {nominations.map((movie, index) => (
+              <MovieDetails
+                title={movie.Title}
+                year={movie.Year}
+                key={movie.imdbD}
+                list="nominations"
+              />
+            ))}
           </ul>
         </div>
       </div>
@@ -53,9 +68,8 @@ const Dashboard = () => {
 export default Dashboard;
 
 const DashboardStyles = styled.div`
-
-text-align: left;
-margin-top: 70px;
+  text-align: left;
+  margin-top: 70px;
 
   h2 {
     margin-bottom: 10px;
@@ -78,38 +92,37 @@ margin-top: 70px;
     }
 
     input {
-    background-image: url(${lens});
-    background-size: 30px;
-    background-color: white;
-    background-repeat: no-repeat;
-    background-position: center left;
-    outline: none;
-    padding: 10px 30px;
-    display: inline-block;
-    width: 100%;
-    border: 1px solid #999;
-    border-radius: 4px;
-    font-size: 1rem;
+      background-image: url(${lens});
+      background-size: 30px;
+      background-color: white;
+      background-repeat: no-repeat;
+      background-position: center left;
+      outline: none;
+      padding: 10px 30px;
+      display: inline-block;
+      width: 100%;
+      border: 1px solid #999;
+      border-radius: 4px;
+      font-size: 1rem;
     }
   }
 
   .actions-area {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 20px;
 
-      div {
-        flex-basis: 49%;
-        background-color: white;
-        padding: 1rem;
-        border: 1px solid #999;
-        border-radius: 4px;
+    div {
+      flex-basis: 49%;
+      background-color: white;
+      padding: 1rem;
+      border: 1px solid #999;
+      border-radius: 4px;
 
-        p {
-          margin-bottom: 15px;
-        }
+      p {
+        margin-bottom: 15px;
       }
     }
-  
-`
+  }
+`;
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 function MovieDetails(props) {
-    return (
-        <MovieDetailsStyles>
-            <span>Movie Name </span>
-            <span>(1999) </span>
-            <button>Nominate</button>
-        </MovieDetailsStyles>    
-    )
+  const handleClick = (e) => {
+    console.log(e.target.outerText);
+  };
+
+  return (
+    <MovieDetailsStyles>
+      <span>{props.title} </span>
+      <span>{props.year} </span>
+      <button onClick={handleClick}>
+        {props.list === "results" ? "Nominate" : "Remove"}
+      </button>
+    </MovieDetailsStyles>
+  );
 }
 
-export default MovieDetails
+export default MovieDetails;
 
 const MovieDetailsStyles = styled.li`
+  margin-top: 5px;
 
-    margin-top: 5px;
+  list-style-type: disc;
+  margin-left: 40px;
 
-    list-style-type: disc;
-    margin-left: 40px;
-
-    button {
-        padding: 4px 10px;
-    }
-
-
-`
+  button {
+    padding: 4px 10px;
+  }
+`;
